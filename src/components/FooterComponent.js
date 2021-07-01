3e5318ff1b206d20bd7a2c2e6906e13e4e5dfc77
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-07-01 12:17:51 +0200

    Footer -> Container fluid

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import { Row, Col, Button } from 'reactstrap';
+import { Row, Col, Button, Container } from 'reactstrap';
 import up from '../images/up.png'
 
 const FooterComponent = () => {
     return (
+        <Container fluid>
+            <Row style={{ textAlign: "center", backgroundColor: "#77777F", height: "140px" }}>
+                <Col className="mt-3">
+                    <br />
+                    <p className="text-white">H F H</p>
+                </Col>
+                <Col className="mt-3">
 
-        <Row style={{ textAlign: "center", backgroundColor: "#77777F", height: "140px" }}>
-            <Col className="mt-3">
-                <br />
-                <p className="text-white">H F H</p>
-            </Col>
-            <Col className="mt-3">
+                    <a href="#Oben" style={{ textDecoration: "none" }}><img src={up} /></a>
 
-                <a href="#Oben" style={{ textDecoration: "none" }}><img src={up} /></a>
-
-            </Col>
-            <Col className="mt-3">
-                <br />
-                <p className="text-white">R e f u g e e k s</p>
-            </Col>
-        </Row>
+                </Col>
+                <Col className="mt-3">
+                    <br />
+                    <p className="text-white">R e f u g e e k s</p>
+                </Col>
+            </Row>
+        </Container>
 
     );
 }
